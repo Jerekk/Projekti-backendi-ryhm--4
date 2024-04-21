b7be0b99e22b442b966008daa4cedebe5e674fb3
vernerss1 <verneri.solonen@metropolia.fi>
2024-04-21 15:54:38 +0300

    kubios kirjautuminen

--- db/create-db-user-example.sql
+++ db/create-db-user-example.sql
@@ -1,7 +1,3 @@
--- CREATE USER 'vernerss'@'localhost' IDENTIFIED BY 'l44k252';
--- GRANT ALL PRIVILEGES ON `healthdiary`.* TO 'vernerss'@'localhost';
--- FLUSH PRIVILEGES;
-
-CREATE USER 'vernerss'@'localhost' IDENTIFIED BY 'l44k252';
-GRANT ALL PRIVILEGES ON `terveyssovellus`.* TO 'vernerss'@'localhost';
+CREATE USER 'myusername'@'localhost' IDENTIFIED BY 'mypassword';
+GRANT ALL PRIVILEGES ON `databasename`.* TO 'myusername'@'localhost';
 FLUSH PRIVILEGES;
