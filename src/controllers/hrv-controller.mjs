4e49db7a91e8f3ce99a1af8638942995e3132cc3
vernerss1 <verneri.solonen@metropolia.fi>
2024-04-29 12:24:05 +0300

    Hrv-commit

---
+++ src/controllers/hrv-controller.mjs
@@ -0,0 +1,16 @@
+import {
+    listAllHrv
+   } from '../models/hrv-model.mjs';
+
+
+ const getHrv = async (req, res) => {
+     const result = await listAllHrv();
+     console.log(result);
+     if (result.error) {
+       return res.status(result.error).json(result.message);
+     }
+
+     return res.json(result);
+   };
+
+ export {getHrv};
